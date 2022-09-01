9b5b180d6ca0a3af006c9d020a1a4f3928ecdfa9
Quinn <Quinn.GitHub@gmail.com>
2022-08-31 20:05:07 -0500

    Update solution.py

--- find-the-access-codes/solution.py
+++ find-the-access-codes/solution.py
@@ -1,2 +1,18 @@
+
+# This technically works, although it takes about 1 min for 2000 random numbers between 1 and 4 inclusive.
+# So I rewrote it in java, see Solution.java
 def solution(l):
-    # Your code here
+	divisible = []
+
+	count = 0
+	for i in range(len(l)):
+		n = l[i]
+		for n2 in divisible:
+			if n % n2 == 0:
+				count += 1
+		j = 0
+		while j <= i-1:
+			if n % l[j] == 0:
+				divisible.append(n)
+			j+=1
+	return count
